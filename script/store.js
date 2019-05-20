44cb0077d32388d725e1cd492be1bee7eab4f83c
Sanjay Kumar Singh01 <sanjay.singh01@irissoftware.com>
2019-05-20 12:52:13 +0530

    final data updated

---
+++ script/store.js
@@ -0,0 +1,68 @@
+var all_Product_1 = document.getElementById("all_Product_1");
+var all_Product_2 = document.getElementById("all_Product_2");
+var all_Product_3 = document.getElementById("all_Product_3");
+var all_Product_4 = document.getElementById("all_Product_4");
+var Sticker = document.getElementById("Sticker");
+var hat = document.getElementById("hat");
+
+var allProduct = document.getElementById("allProduct");
+var hatProduct = document.getElementById("hatProduct");
+var stickerProduct = document.getElementById("stickerProduct");
+var topwareProduct = document.getElementById("topwareProduct");
+
+
+
+function allProducts(){
+  Sticker.style.display = "block";
+  hat.style.display = "block";
+  all_Product_1.style.display = "block";
+  all_Product_2.style.display = "block";
+  all_Product_3.style.display = "block";
+  all_Product_4.style.display = "block";
+  allProduct.classList.add("active");
+  hatProduct.classList.remove("active");
+  stickerProduct.classList.remove("active");
+
+}
+
+function Stickers(){
+  Sticker.style.display = "block";
+  hat.style.display = "none";
+  all_Product_1.style.display = "none";
+  all_Product_2.style.display = "none";
+  all_Product_3.style.display = "none";
+  all_Product_4.style.display = "none";
+
+  allProduct.classList.remove("active");
+  hatProduct.classList.remove("active");
+  stickerProduct.classList.add("active");
+
+}
+
+function Hats(){
+  Sticker.style.display = "none";
+  hat.style.display = "block";
+  all_Product_1.style.display = "none";
+  all_Product_2.style.display = "none";
+  all_Product_3.style.display = "none";
+  all_Product_4.style.display = "none";
+
+  allProduct.classList.remove("active");
+  hatProduct.classList.add("active");
+  stickerProduct.classList.remove("active");
+
+}
+
+function Topware(){
+  Sticker.style.display = "none";
+  hat.style.display = "block";
+  all_Product_1.style.display = "block";
+  all_Product_2.style.display = "block";
+  all_Product_3.style.display = "block";
+  all_Product_4.style.display = "block";
+
+  allProduct.classList.remove("active");
+  hatProduct.classList.remove("active");
+  stickerProduct.classList.remove("active");
+  topwareProduct.classList.add("active");
+}
